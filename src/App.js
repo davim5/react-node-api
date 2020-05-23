d224fd8b2bd9a9ccec4bee121811173c7c4a6ce4
davim5 <davim53@hotmail.com>
2020-05-23 10:38:19 -0300

    adicionando e excluindo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Teste Novo",
+      url: "site.com.br",
+      techs: [
+        "node",
+        "react",
+        "react native"
+      ],
+    });
+
+    setRepos([...repos, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   
+    await api.delete(`repositories/${id}`);
+
+    const newRepo = repos.filter(p=>p.id!==id);
+
+    setRepos(newRepo)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+           </button>
+          </li>
+        ))}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
